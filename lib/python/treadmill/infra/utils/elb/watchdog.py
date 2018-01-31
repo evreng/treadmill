f8cce77b3586a6590db6b2e4f14dead742128e62
Evren <evreng@users.noreply.github.com>
2018-01-31 12:05:30 +0000

    elb.watchdog: added threaded execution, bug fixing

--- lib/python/treadmill/infra/utils/elb/watchdog.py
+++ lib/python/treadmill/infra/utils/elb/watchdog.py
@@ -1,13 +1,35 @@
+'''Run in background:
+    elbManagerDaemon = EndpointWatcher(ZKFS_DIR)
+    elbManagerDaemon.start()
+
+or in the foreground:
+    elbManagerDaemon.run()
+'''
 from treadmill.infra.utils.elb.manager import ELBManager
 from treadmill.dirwatch import DirWatcher
 from types import SimpleNamespace
 import os
 import re
 import logging
-from threading import Thread
+from threading import Event, Thread
 
 ZKFS_DIR = '/tmp/zkfs-test'
 _LOGGER = logging.getLogger(__name__)
+
+
+class EndpointWatcherThread(Thread):
+    def __init__(self, target, name="ELB Manager Watchdog"):
+        self._stop_event = Event()
+        Thread.__init__(self, target=target, name=name, daemon=True)
+
+    def stop(self):
+        self._stop_event.set()
+        Thread.join(self, timeout=0)
+        _LOGGER.info("{} has stopped".format(self.name))
+
+    def start(self):
+        _LOGGER.info("{} has started".format(self.name))
+        Thread.start(self)
 
 
 class EndpointWatcher(ELBManager):
@@ -15,12 +37,16 @@ class EndpointWatcher(ELBManager):
         ELBManager.__init__(self, zkfs_dir)
         self.on_created = self._on_created
         self.on_deleted = self._on_deleted
-
+        self.thread = None
+        
     def get_context(self, path):
         c = SimpleNamespace()
         c.proid, c.fileName = path.split('/')[-2:]
-        c.app, c.cell, c.protocol = re.search('([a-z0-9-]+)\.([a-z0-9-]+)#\d*:\w+:(\w+)', c.fileName).groups()
-        c.lb_name = "{}-{}-{}-{}".format(c.proid, c.app, c.cell, c.protocol)
+        try:
+            c.app, c.cell, c.protocol = re.search('([a-z0-9-]+)\.([a-z0-9-]+)#\d*:\w+:(\w+)', c.fileName).groups()
+        except:
+            return None
+        c.tg_name = "{}-{}-{}-{}".format(c.proid, c.app, c.cell, c.protocol)
         c.fileRegex = '{}.{}#\d+:.*:{}'.format(c.app, c.cell, c.protocol)
         c.proid_dir = '{}/{}'.format(self.endpoint_dir, c.proid)
         return c
@@ -38,49 +64,64 @@ class EndpointWatcher(ELBManager):
 
     def _on_created(self, path):
         context = self.get_context(path)
-        print("Found new endpoint {}/{}".format(context.proid_dir, context.fileName))
+        _LOGGER.info("Found new endpoint {}/{}".format(context.proid_dir, context.fileName))
         targets = [self.getTargetFromFile(path)]
-        self.register(context.lb_name, targets)
-        print("{} created".format(context.lb_name))
-        tg = self.findTargetGroup(name=context.lb_name)
+        self.register(context.proid, context.tg_name, targets)
+        tg = self.findTargetGroup(name=context.tg_name)
         if tg:
             self.add_targets(tg, targets)
-        print("Processed adding of endpoint {}".format(targets))
+            self.update_app_groups(path, tg)
+        _LOGGER.info("Processed adding of endpoint {}".format(targets))
 
     def _on_deleted(self, path):
         context = self.get_context(path)
-        print("Deleted endpoint {}/{}".format(context.proid_dir, context.fileName))
-        targets = self.getEndPoints(path)
-        tg = self.findTargetGroup(name=context.lb_name)
-        if tg and targets:
-            self.remove_targets(tg, targets)
-            print("Processed removing of endpoint {}".format(path))
-        else:
-            self.deregister(context.lb_name)
-            print("Load balancer {} has been removed (no targets)".format(context.lb_name))
+        if not context:
+            return
+        _LOGGER.info("Deleted endpoint {}/{}".format(context.proid_dir, context.fileName))
+        leftTargets = self.getEndPoints(path)
+        tg = self.findTargetGroup(name=context.tg_name)
+        if not tg:
+            return
+        targets = self.remove_targets(tg, leftTargets)
+        _LOGGER.info("Processed removing of endpoint {}".format(targets))
+        if not leftTargets:
+            self.deregister(context.tg_name)
+        self.update_app_groups(path, tg)
 
-    def _on_modified(self, path):
-        self._on_created(path)
-        self._on_deleted(path)
+
+    def update_app_groups(self, path, target_group):
+        context = self.get_context(path)
+        elb = self.findLoadBalancer(name=context.proid)
+        elb_dns = elb.dnsName if elb else '<load_balancer_not_found>'
+        with open('{}/{}.{}.{}'.format(self.app_groups_dir, context.proid, context.app, context.cell), 'w') as app_group:
+            app_group.write('''cells: [{c.cell}]
+            \rdata: ['virtuals={dns}.{t.port}',
+            \r  environment=dev, 'vips={dns}',
+            \r  port={t.port}]
+            \rendpoints: [{c.protocol}]
+            \rgroup-type: lbendpoint
+            \rpattern: {c.proid}.{c.app}'''.format(c=context, t=target_group, dns=elb_dns))
 
     def run(self):
+        """Use "start" to run it as a background daemon or "run" to run it in the foreground"""
         watch = DirWatcher()
         watch.on_created = self._on_created
         watch.on_deleted = self._on_deleted
-        watch.on_modified = self._on_modified
+        # watch.on_modified = self._on_modified
         while True:
-            apps = list(set([d.split('.')[0] for d in os.listdir(self.endpoint_dir)]))
-            for app in apps:
-                if not app:
-                    continue
-                appdir = "/".join((self.endpoint_dir, app))
-                watch.add_dir(appdir)
+            watch._watches = {}
+            [watch.add_dir("/".join((self.endpoint_dir, app))) for app in os.listdir(self.endpoint_dir)]
             if watch.wait_for_events(5):
                 watch.process_events()
 
+    def start(self):
+        """Use "start" to run it as a background daemon or "run" to run it in the foreground"""
+        self.thread = EndpointWatcherThread(target=self.run)
+        self.thread.start()
 
-apw = EndpointWatcher(ZKFS_DIR)
-t = Thread(target=apw.run)
-t.daemon = True
-t.start()
+    def stop(self):
+        """Use "stop" to kill the background daemon"""
+        if self.thread:
+            self.thread.stop()
+        self.thread = None
 
